fe44be2ed53f85e01bf7fed1919bb7c36e424c5b
Nishanth-Emu <nishanth6504@gmail.com>
2025-05-28 17:02:27 +0530

    auth-route-post-api-tested-working-fine

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,24 +1,24 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcryptjs';
-import User from '../models/User'; // Import the User model
+import User from '../models/User'; 
 
 // Register a new user
 export const registerUser = async (req: Request, res: Response): Promise<void> => {
   const { username, email, password, role } = req.body;
 
   try {
-    // 1. Check if user already exists
+    // Check if user already exists
     const existingUser = await User.findOne({ where: { email } });
     if (existingUser) {
        res.status(409).json({ message: 'User with this email already exists.' });
        return
     }
 
-    // 2. Hash the password
+    // Hash the password
     const salt = await bcrypt.genSalt(10); // Generate a salt (random string)
-    const passwordHash = await bcrypt.hash(password, salt); // Hash the password with the salt
+    const passwordHash = await bcrypt.hash(password, salt); 
 
-    // 3. Create the new user in the database
+    
     const newUser = await User.create({
       username,
       email,
@@ -26,7 +26,7 @@ export const registerUser = async (req: Request, res: Response): Promise<void> =
       role: role || 'Viewer', // Use provided role or default to 'Viewer'
     });
 
-    // 4. Send a success response (excluding password hash for security)
+    // Send a success response (excluding password hash for security)
     res.status(201).json({
       message: 'User registered successfully',
       user: {
